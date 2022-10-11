fdc8646db28043ae842a5055236f37a86ecbdb72
David Angell <davidjangell42@gmail.com>
2022-10-11 00:19:16 -0400

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from 'astro/config'
+import svelte from '@astrojs/svelte'
+import mdx from '@astrojs/mdx'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx(), svelte()],
+  markdown: {
+    shikiConfig: {
+      theme: 'nord',
+    },
+    rehypePlugins: [
+      [
+        'rehype-external-links',
+        {
+          target: '_blank',
+        },
+      ],
+    ],
+  },
+})
