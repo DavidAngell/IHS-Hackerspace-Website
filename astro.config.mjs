9e09131801ad47b71ccccf58944883406331f916
David Angell <davidjangell42@gmail.com>
2022-10-12 17:07:09 -0400

    slowly making progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,11 @@ import mdx from '@astrojs/mdx'
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), svelte()],
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
   markdown: {
     shikiConfig: {
       theme: 'nord',
