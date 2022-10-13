c0bfabf50d05cd17aac2763f8f7595af66bd46ed
David Angell <davidjangell42@gmail.com>
2022-10-13 15:09:04 -0400

    added placeholder projects page

---
+++ src/data/projects-data.js
@@ -0,0 +1,41 @@
+export const ongoingProjects = [
+    {
+        name: "placeholder",
+        description: "placeholder",
+        image: "/assets/projects/david.png",
+        blogPost: "/blog/hello-world",
+    },
+    {
+        name: "placeholder",
+        description: "placeholder",
+        image: "/assets/projects/david.png",
+        blogPost: "/blog/hello-world",
+    },
+    {
+        name: "placeholder",
+        description: "placeholder",
+        image: "/assets/projects/david.png",
+        blogPost: "/blog/hello-world",
+    },
+    {
+        name: "placeholder",
+        description: "placeholder",
+        image: "/assets/projects/david.png",
+        blogPost: "/blog/hello-world",
+    },
+    {
+        name: "placeholder",
+        description: "placeholder",
+        image: "/assets/projects/david.png",
+        blogPost: "/blog/hello-world",
+    },
+]
+
+export const pastProjects = [
+    {
+        name: "placeholder",
+        description: "placeholder",
+        image: "/assets/projects/david.png",
+        blogPost: "/blog/hello-world",
+    }
+]
