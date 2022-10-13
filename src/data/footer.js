f23b29158e7038c2d5c589885cd3031f0806a766
David Angell <davidjangell42@gmail.com>
2022-10-13 19:16:54 -0400

    added links to footer

---
+++ src/data/footer.js
@@ -0,0 +1,8 @@
+export const footerIconLinks = {
+    // Must be named either something from the mdi library or a custom icon in the /src/icons folder
+    // Ex. src/icons/discord.svg is the saved icon for discord
+    'mdi:github': "https://github.com/DavidAngell",
+    'mdi:twitter': "https://twitter.com/",
+    'mdi:instagram': "https://instagram.com/",
+    'discord': "https://discord.gg/rbjsQWQBGB", //stored in /src/icons/discord.svg
+}
