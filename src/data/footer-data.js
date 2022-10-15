07f8f4f669862b970cdb1b02c9c328864d0d8d83
David Angell <davidjangell42@gmail.com>
2022-10-14 22:57:48 -0400

    more links

--- src/data/footer-data.js
+++ src/data/footer-data.js
@@ -1,8 +1,8 @@
 export const footerIconLinks = {
     // Must be named either something from the mdi library or a custom icon in the /src/icons folder
     // Ex. src/icons/discord.svg is the saved icon for discord
-    'mdi:github': "https://github.com/DavidAngell",
-    'mdi:twitter': "https://twitter.com/",
-    'mdi:instagram': "https://instagram.com/",
+    'mdi:github': "https://github.com/ihs-hackerspace",
+    'mdi:twitter': "https://twitter.com/IHSHackerspace",
+    'mdi:instagram': "https://www.instagram.com/ihs.hackerspace/",
     'discord': "https://discord.gg/rbjsQWQBGB", //stored in /src/icons/discord.svg
 }
