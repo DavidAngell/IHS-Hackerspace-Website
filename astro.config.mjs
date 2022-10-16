cc15d88dbbed64ebf6757fec5429ec85ba838ff7
David Angell <davidjangell42@gmail.com>
2022-10-15 20:02:54 -0400

    chaged around many imports and added firebase support for later

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
 import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 import mdx from '@astrojs/mdx'
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), svelte()],
+  // sitemap: true,
   vite: {
     ssr: {
       external: ["svgo"],
+    },
+    resolve: {
+      alias: {
+        '@components': path.resolve('./src/components'),
+      },
+      preserveSymlinks: true
     },
   },
   markdown: {
@@ -23,5 +31,5 @@ export default defineConfig({
       ],
     ],
   },
-  site: 'https://www.my-site.dev',
+  site: 'https://localhost:5000/',
 })
