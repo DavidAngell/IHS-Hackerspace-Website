e14083be1a9472628b2e0a8c3f434b93f9ae5533
David Angell <davidjangell42@gmail.com>
2022-10-16 21:18:25 -0400

    added new dumb component

--- src/data/homepage-data.js
+++ src/data/homepage-data.js
@@ -1,47 +1,47 @@
 export const homepageImageCarousel = [
-    {
-        title: 'Machine Learning',
-        description: 'Learn to make neural networks and apply them in fun and engaging ways!',
-        images: [
-            '/assets/home/IMG_9390.jpeg',
-            '/assets/home/IMG_9398.jpeg',
-            '/assets/home/PXL_20211115_223443718.MP.jpg',
-        ],
-    },
-    {
-        title: 'Cyber Security',
-        description: 'Learn how pentesters break into systems and participate in CTF competitions to test your knowledge and teamwork',
-        images: [
-            '/assets/home/IMG_9390.jpeg',
-            '/assets/home/IMG_9398.jpeg',
-            '/assets/home/PXL_20211115_223443718.MP.jpg',
-        ],
-    },
-    {
-        title: 'Arduino',
-        description: 'Discover how basic circuits are made and learn to program Wifi-controlled outlets, electronic locks, and just about anything you can imagine!',
-        images: [
-            '/assets/home/IMG_9390.jpeg',
-            '/assets/home/IMG_9398.jpeg',
-            '/assets/home/PXL_20211115_223443718.MP.jpg',
-        ],
-    },
-    {
-        title: 'Open Environment',
-        description: 'Come explore whatever you want! We\'ll give you the tools to do so!',
-        images: [
-            '/assets/home/IMG_9390.jpeg',
-            '/assets/home/IMG_9398.jpeg',
-            '/assets/home/PXL_20211115_223443718.MP.jpg',
-        ],
-    },
-    {
-        title: 'Teach Others',
-        description: 'Come teach about what you have learned in your own exploration and share it with everyone!',
-        images: [
-            '/assets/home/IMG_9390.jpeg',
-            '/assets/home/IMG_9398.jpeg',
-            '/assets/home/PXL_20211115_223443718.MP.jpg',
-        ],
-    },
+	{
+		title: 'Machine Learning',
+		description: 'Learn to make neural networks and apply them in fun and engaging ways!',
+		images: [
+			'/assets/home/IMG_9390.jpeg',
+			'/assets/home/IMG_9398.jpeg',
+			'/assets/home/PXL_20211115_223443718.MP.jpg',
+		],
+	},
+	{
+		title: 'Cyber Security',
+		description: 'Learn how pentesters break into systems and participate in CTF competitions to test your knowledge and teamwork',
+		images: [
+			'/assets/home/IMG_9390.jpeg',
+			'/assets/home/IMG_9398.jpeg',
+			'/assets/home/PXL_20211115_223443718.MP.jpg',
+		],
+	},
+	{
+		title: 'Arduino',
+		description: 'Discover how basic circuits are made and learn to program Wifi-controlled outlets, electronic locks, and just about anything you can imagine!',
+		images: [
+			'/assets/home/IMG_9390.jpeg',
+			'/assets/home/IMG_9398.jpeg',
+			'/assets/home/PXL_20211115_223443718.MP.jpg',
+		],
+	},
+	{
+		title: 'Open Environment',
+		description: 'Come explore whatever you want! We\'ll give you the tools to do so!',
+		images: [
+			'/assets/home/IMG_9390.jpeg',
+			'/assets/home/IMG_9398.jpeg',
+			'/assets/home/PXL_20211115_223443718.MP.jpg',
+		],
+	},
+	{
+		title: 'Teach Others',
+		description: 'Come teach about what you have learned in your own exploration and share it with everyone!',
+		images: [
+			'/assets/home/IMG_9390.jpeg',
+			'/assets/home/IMG_9398.jpeg',
+			'/assets/home/PXL_20211115_223443718.MP.jpg',
+		],
+	},
 ]
